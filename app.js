4afac59f0a0085a0a5e0af6fea19b5df05d3d0fa
gernesto14 <gernesto14@yahoo.com>
2025-01-15 17:45:21 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,49 @@
+import "dotenv/config";
+
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+
+const app = express();
+
+// view engine setup
+app.set(
+  "views",
+  path.join(path.dirname(new URL(import.meta.url).pathname), "views")
+);
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  express.static(
+    path.join(path.dirname(new URL(import.meta.url).pathname), "public")
+  )
+);
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+export default app;
